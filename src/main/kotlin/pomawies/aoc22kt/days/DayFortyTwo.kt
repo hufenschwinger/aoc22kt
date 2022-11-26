66bc34a7d555583401b618972feed1474efcf898
Mark Wiese <mark.wiese@stud.hs-ruhrwest.de>
2022-11-26 14:56:57 +0100

    fetch input

--- src/main/kotlin/pomawies/aoc22kt/days/DayFortyTwo.kt
+++ src/main/kotlin/pomawies/aoc22kt/days/DayFortyTwo.kt
@@ -1,15 +1,15 @@
 package pomawies.aoc22kt.days
 
-class DayFortyTwo : IDay {
+class DayFortyTwo : IDay() {
     override fun number(): Int {
         return 42
     }
 
     override fun partOne(): Long {
-        return 1L
+        return 1L * inputOne().size
     }
 
     override fun partTwo(): Long {
-        return 2L
+        return 2L * inputTwo().size
     }
 }
--- src/main/kotlin/pomawies/aoc22kt/days/IDay.kt
+++ src/main/kotlin/pomawies/aoc22kt/days/IDay.kt
@@ -1,7 +1,33 @@
 package pomawies.aoc22kt.days
 
-interface IDay {
-    fun number(): Int
-    fun partOne(): Long
-    fun partTwo(): Long
+import java.nio.file.Files
+import java.nio.file.Path
+
+private const val srcRoot = "src/main/resources/"
+
+abstract class IDay {
+    private fun getInput(part: Int): List<String> {
+        return Files.readAllLines(
+            Path.of(srcRoot)
+                .resolve(
+                    String.format(
+                        "D%dP%d.txt",
+                        number(),
+                        part
+                    )
+                )
+        )
+    }
+
+    protected fun inputOne(): List<String> {
+        return getInput(1)
+    }
+
+    protected fun inputTwo(): List<String> {
+        return getInput(2)
+    }
+
+    abstract fun number(): Int
+    abstract fun partOne(): Long
+    abstract fun partTwo(): Long
 }
